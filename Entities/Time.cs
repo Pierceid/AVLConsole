b0761275ca5c52ab09b0f59cb73ebb2030049d28
Pierceid <128907409+Pierceid@users.noreply.github.com>
2025-10-30 23:05:53 +0100

    updated gui and reports

--- Entities/Time.cs
+++ Entities/Time.cs
@@ -3,22 +3,27 @@
 namespace AVLConsole.Entities {
     public class Time : Item, IKey<Time> {
         public DateTime Value { get; set; }
+        public int Code { get; set; }
 
         public Time() {
             Value = DateTime.Now;
+            Code = 0;
         }
 
-        public Time(DateTime value) {
+        public Time(DateTime value, int code) {
             Value = value;
+            Code = code;
         }
 
         public int Compare(Time other) {
-            return Util.CompareTimes(Value, other.Value);
+            int cmp = Util.CompareTimes(Value, other.Value);
+            return cmp == 0 ? Util.CompareNumbers(Code, other.Code) : cmp;
         }
 
         public Time ImportKeys(string line) {
-            if (DateTime.TryParse(line, out DateTime parsedValue)) {
-                return new Time(parsedValue);
+            string[] parts = line.Split(',');
+            if (DateTime.TryParse(parts[0], out DateTime parsedValue) && int.TryParse(parts[1], out int parsedCode)) {
+                return new Time(parsedValue, parsedCode);
             }
             throw new FormatException($"Invalid key format: {line}");
         }
@@ -28,14 +33,15 @@ namespace AVLConsole.Entities {
         }
 
         public override Item ImportItem(string line) {
-            if (DateTime.TryParse(line, out DateTime parsedValue)) {
-                return new Time(parsedValue);
+            string[] parts = line.Split(',');
+            if (DateTime.TryParse(parts[0], out DateTime parsedValue) && int.TryParse(parts[1], out int parsedCode)) {
+                return new Time(parsedValue, parsedCode);
             }
             throw new FormatException($"Invalid item format: {line}");
         }
 
         public override string ExportItem() {
-            return Value.ToString(Constants.DATETIME_FORMAT);
+            return $"{Value.ToString(Constants.DATETIME_FORMAT)},{Code}";
         }
     }
 
